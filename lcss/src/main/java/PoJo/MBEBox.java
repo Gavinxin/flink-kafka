2bdc8cb60b1ec04a89101847f1a2550d6bdbce90
Gavin <Gavin@DESKTOP-VASOCUS>
2018-11-06 16:41:47 +0800

    MBEBox

---
+++ lcss/src/main/java/PoJo/MBEBox.java
@@ -0,0 +1,47 @@
+package PoJo;
+
+public class MBEBox {
+	private long lowerTime;
+	private long upperTime;
+	private double lowerLongitude;
+	private double upperLongitude;
+	private double lowerLatitude;
+	private double upperLatitude;
+	public long getLowerTime() {
+		return lowerTime;
+	}
+	public void setLowerTime(long lowerTime) {
+		this.lowerTime = lowerTime;
+	}
+	public long getUpperTime() {
+		return upperTime;
+	}
+	public void setUpperTime(long upperTime) {
+		this.upperTime = upperTime;
+	}
+	public double getLowerLongitude() {
+		return lowerLongitude;
+	}
+	public void setLowerLongitude(double lowerLongitude) {
+		this.lowerLongitude = lowerLongitude;
+	}
+	public double getUpperLongitude() {
+		return upperLongitude;
+	}
+	public void setUpperLongitude(double upperLongitude) {
+		this.upperLongitude = upperLongitude;
+	}
+	public double getLowerLatitude() {
+		return lowerLatitude;
+	}
+	public void setLowerLatitude(double lowerLatitude) {
+		this.lowerLatitude = lowerLatitude;
+	}
+	public double getUpperLatitude() {
+		return upperLatitude;
+	}
+	public void setUpperLatitude(double upperLatitude) {
+		this.upperLatitude = upperLatitude;
+	}
+	
+}
